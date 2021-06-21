8653406d41e9c7c93e34cfb49cb0f1b7e619ba3b
Yash Brahmbhatt <yash.brahmb@gmail.com>
2021-06-21 18:37:44 -0400

    Added welcome message and todo

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello World !!");
+
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+
+        //TODO - Create a loop to continue the selection until EXIT option is selected
+            System.out.println("---------------- Welcome to your TODO list ----------------");
+            System.out.print("Select an option\n1. Create a new task\n2. View a task\n3. Update a task\n4. Delete a task\n5. Exit\n~ ");
+            choice = sc.nextInt();
+            System.out.println("You have selected " + choice);
+
+
+
     }
 }
