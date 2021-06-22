7e5187afbfa2d2a9322c2fd1c0aef31d5660e5a0
preet329 <61388047+preet329@users.noreply.github.com>
2021-06-22 17:48:04 -0400

    implemented option loop

--- src/Runner.java
+++ src/Runner.java
@@ -6,13 +6,35 @@ public class Runner {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
 
-        //TODO - Create a loop to continue the selection until EXIT option is selected
+        while (choice != 5) {
             System.out.println("---------------- Welcome to your TODO list ----------------");
             System.out.print("Select an option\n1. Create a new task\n2. View a task\n3. Update a task\n4. Delete a task\n5. Exit\n~ ");
             choice = sc.nextInt();
             System.out.println("You have selected " + choice);
 
+            switch (choice) {
 
+                case 1:
+                    // addTasks(null);
+                    break;
 
+                case 2:
+                    // getTasksbyId(1);
+                    break;
+
+                case 3:
+                    // updateTasks(null);
+                    break;
+
+                case 4:
+                    // deleteTasks(null);
+                    break;
+
+                default:
+                    choice = 5;
+                    break;
+
+            }
+        }
     }
 }
