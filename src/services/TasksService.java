6478664481dfdc78ce132db42cedea1d62c095b0
Yash Brahmbhatt <yash.brahmb@gmail.com>
2021-06-22 18:13:16 -0400

    Modified task services

---
+++ src/services/TasksService.java
@@ -0,0 +1,12 @@
+package services;
+
+import models.Tasks;
+
+import java.util.List;
+
+public interface TasksService {
+    public Tasks getTasksById(List<Tasks> tasksList, int id);
+    public List<Tasks> addTasks(List<Tasks> tasksList, Tasks task);
+    public List<Tasks> updateTasks(List<Tasks> tasksList, Tasks task);
+    public List<Tasks> deleteTasks(List<Tasks> tasksList, Tasks task);
+}
--- src/services/impl/TasksServiceImpl.java
+++ src/services/impl/TasksServiceImpl.java
@@ -1,8 +1,30 @@
 package services.impl;
 
-import services.impl.services.TasksService;
+import models.Tasks;
+import services.TasksService;
+
+import java.util.List;
 
 public class TasksServiceImpl implements TasksService {
     // TODO implement abstract methods
 
+    @Override
+    public Tasks getTasksById(List<Tasks> tasksList, int id) {
+        return null;
+    }
+
+    @Override
+    public List<Tasks> addTasks(List<Tasks> tasksList, Tasks task) {
+        return null;
+    }
+
+    @Override
+    public List<Tasks> updateTasks(List<Tasks> tasksList, Tasks task) {
+        return null;
+    }
+
+    @Override
+    public List<Tasks> deleteTasks(List<Tasks> tasksList, Tasks task) {
+        return null;
+    }
 }
