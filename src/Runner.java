b81451909a9be0077e87035660a06324559212a6
preet329 <61388047+preet329@users.noreply.github.com>
2021-06-30 19:02:31 -0400

    Implemented update and delete

--- src/Runner.java
+++ src/Runner.java
@@ -12,6 +12,7 @@ public class Runner {
         Scanner sc = new Scanner(System.in);
         List<Tasks> database = new ArrayList<>();
         TasksServiceImpl tasksService = new TasksServiceImpl();
+        Tasks t;
 
         System.out.println("---------------- Welcome to your TODO list ----------------");
         System.out.print("Select an option\n1. Create a new task\n2. View a task\n3. Update a task\n4. Delete a task\n5. Exit\n~ ");
@@ -24,9 +25,11 @@ public class Runner {
                 case 1:
                     System.out.print("What's on your mind? -> ");
                     // TODO get user input and add it to database
-                    String new_Task = sc.next();
-                    //TasksServiceImpl task = new TasksServiceImpl();
-                    System.out.println(new_Task);
+                    String new_Title = sc.next();
+                    System.out.println(new_Title);
+
+                     t = new Tasks(new_Title);
+                    database = tasksService.addTasks(database, t);
 
                     System.out.println("Task has been added. Total tasks: " + database.size());
                     break;
@@ -36,18 +39,26 @@ public class Runner {
                     // TODO print the task which user wants
                     int id_number = sc.nextInt();
                     System.out.println(id_number);
+
+                    t = tasksService.getTasksById(database, id_number);
+
+                    System.out.println("This is your task: " + t);
                     break;
 
                 case 3:
                     // TODO Ask user to update title for now and print updated tasks at the end
                     System.out.println("Enter a Task Id to Update Task: ");
                     int id_find = sc.nextInt();
+
+                     t = tasksService.getTasksById(database, id_find);
+
                     System.out.println("Enter new title to update the task: ");
-                    String new_Title = sc.next();
+                    String update_Title = sc.next();
 
-                    System.out.println(id_find);
-                    System.out.println(new_Title);
+                    t.setTitle(update_Title);
+                     database = tasksService.updateTasks(database, t);
 
+                    System.out.println("Here is the updated Task: " + t);
                     break;
 
                 case 4:
@@ -55,6 +66,10 @@ public class Runner {
                     System.out.println("Enter a Task Id to delete Task: ");
                     int id_delete = sc.nextInt();
                     System.out.println(id_delete);
+
+                     t = tasksService.getTasksById(database, id_delete);
+
+                     database = tasksService.deleteTasks(database, t);
 
                     System.out.println("The total task in the database is: " + database.size());
                     break;
